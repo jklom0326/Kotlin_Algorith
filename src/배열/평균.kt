49efbd7e6da760f4c8eabe24c5384ef6b402821d
Sangwan Han <jklom0326@gmail.com>
2021-08-19 22:58:38 +0900

    algo

--- src/배열/평균.kt
+++ src/배열/평균.kt
@@ -1,5 +1,18 @@
 package 배열
 
-fun main() {
 
+fun main() {
+    val n = readLine()!!.toInt()
+    val array = readLine()!!.split(" ").map { it.toDouble() }.toMutableList()
+    val max = array.maxOrNull()
+    var sum = 0.0
+    for (i in array.indices){
+        sum += newScore(array[i], max!!.toDouble())
+    }
+    println(sum/n)
+
+}
+
+private fun newScore(score: Double, max: Double): Double {
+    return score / max * 100
 }
