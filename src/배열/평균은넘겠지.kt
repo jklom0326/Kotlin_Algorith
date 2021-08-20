cc7e2a6a8b1945c9673f3a7a038ff95d1cd0dd53
Sangwan Han <jklom0326@gmail.com>
2021-08-20 23:25:28 +0900

    algo

---
+++ src/배열/평균은넘겠지.kt
@@ -0,0 +1,28 @@
+package 배열
+
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val arr = Array(n) { "" }
+    for (i in arr.indices) {
+        val input = readLine()!!.toString()
+        arr[i] = input
+    }
+
+    arr.forEach {
+        val line = it.split(" ").map { it.toInt() }
+        val aver = (line.sumBy { it } - line[0]) / line[0]
+        val human = line[0]
+        var count = 0
+        for (i in line) {
+            if (i == human){
+                continue
+            }
+            if (i > aver) {
+                count++
+            }
+        }
+        val stu = String.format("%.3f", (count.toDouble() / human.toDouble()) * 100)
+        println("$stu%")
+    }
+}
