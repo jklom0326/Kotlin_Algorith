3ce1a3466c9772127510712fbc727c7c0b7532bb
Sangwan Han <jklom0326@gmail.com>
2022-02-15 00:54:38 +0900

    algo

--- src/재귀/별찍기.kt
+++ src/재귀/별찍기.kt
@@ -1,16 +1,34 @@
 package 재귀
 
+lateinit var arr : Array<Array<Char>>
+
 fun main() {
     val n = readLine()!!.toInt()
-    val arr = arrayOf(n, arrayOf(n,{'*'}))
-
-    for (i in 0 until n) {
-        println(arr[i])
+    val builder = StringBuilder()
+    arr = Array(n) { Array(n, { ' ' }) }
+    star(n,0,0)
+    for (i in 0 until n){
+        for (j in 0 until n){
+            builder.append(arr[i][j])
+        }
+        builder.append("\n")
     }
+    print(builder)
 }
 
 fun star(n : Int, x : Int, y : Int) {
     if (n == 1){
+        arr[x][y] = '*'
+        return
+    }
+    val value = n / 3
 
+    for (i in 0 until 3){
+        for (j in 0 until 3){
+            if ((i == 1) && (j == 1))
+                continue
+            else
+                star(value, x + (value * i), y + (value * j))
+        }
     }
 }
