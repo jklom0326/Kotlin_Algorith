bec1acb3d1d4357c79819ae6820764531bac820f
Sangwan Han <jklom0326@gmail.com>
2022-02-20 22:50:47 +0900

    algo

--- src/재귀/하노이탑.kt
+++ src/재귀/하노이탑.kt
@@ -2,18 +2,23 @@ package 재귀
 
 import kotlin.math.pow
 
+val builder = StringBuilder()
+
+
 fun main() {
     val n = readLine()!!.toInt()
-    println((2.0.pow(n.toDouble()) -1).toInt())
+    builder.append((2.0.pow(n.toDouble()) -1).toInt()).append('\n')
     hanoi(n,1,2,3)
+    println(builder)
+
 }
 
 fun hanoi(n:Int, start:Int, mid:Int, to:Int){
     if(n == 1){
-        println("${start} ${to}")
+        builder.append("${start} ${to}\n")
         return
     }
     hanoi(n - 1, start, to, mid)
-    println("${start} ${to}")
+    builder.append("${start} ${to}\n")
     hanoi(n - 1, mid, start, to)
 }
