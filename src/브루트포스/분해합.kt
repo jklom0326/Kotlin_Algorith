64a6cec3aa4b8a337bdbdbb9377749bec7265e80
Sangwan Han <jklom0326@gmail.com>
2022-03-03 02:51:25 +0900

    algo

--- src/브루트포스/분해합.kt
+++ src/브루트포스/분해합.kt
@@ -2,6 +2,21 @@ package 브루트포스
 
 fun main() {
     val n = readLine()!!.toInt()
+    val len = n.toString().length
+    var result = 0
 
+    for (i in (n - (len * 9)) until n){
+        var sum = 0
+        var num = i
 
+        while (num != 0){
+            sum += num % 10
+            num /= 10
+        }
+        if ((sum + i) == n){
+            result = i
+            break
+        }
+    }
+    println(result)
 }
