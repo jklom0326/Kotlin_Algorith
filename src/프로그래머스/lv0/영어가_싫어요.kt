7aefa96898e7319e2d521661832541a0b86118e9
jklom0326 <jklom0326@gmail.com>
2022-10-25 22:52:48 +0900

    programers lv0

--- src/프로그래머스/lv0/영어가_싫어요.kt
+++ src/프로그래머스/lv0/영어가_싫어요.kt
@@ -1,5 +1,14 @@
 package 프로그래머스.lv0
+fun hateEnglish(numbers: String): Long {
+    var answer: Long = 0
+    var number = numbers
+    var numstr = arrayOf("zero","one","two","three","four","five","six","seven","eight","nine")
+    for (i in 0..9){
 
+        number = number.replace(numstr,i.toString())
+    }
+    return answer
+}
 fun main() {
-
+    println(hateEnglish("onetwothreefourfivesixseveneightnine"))
 }
