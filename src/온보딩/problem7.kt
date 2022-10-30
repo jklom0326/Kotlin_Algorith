1cfd7bd8c3f6ce0ebd357ea0b5f65c63041d2a35
jklom0326 <jklom0326@gmail.com>
2022-10-31 05:08:38 +0900

    onboarding 5일차

--- src/온보딩/problem7.kt
+++ src/온보딩/problem7.kt
@@ -1,45 +1,85 @@
 package 온보딩
 
 fun solution7(
-    user: String, friends: List<List<String>>, visitors: List<String>
+    user: String,
+    friends: List<List<String>>,
+    visitors: List<String>
 ): List<String> {
-    println(friends)
     val friendSet = mutableSetOf<String>()
-    val friendMap = mutableMapOf<String, List<String>>()
+    var friendMap = mutableMapOf<String, Int>()
+    var result = listOf<String>()
     // 유저의 친구 목록
     searchFriends(friends, user, friendSet)
-    println(friendSet)
+    // 친구의 친구 점수
+    friendMap = friendsScore(friends, friendSet, user)
+    // 방문자의 점수
+    visitorScore(visitors, friendSet, friendMap)
+    // 점수에 따른 정렬
+    result = sortedByScore(friendMap)
 
-    friends.forEach {
-        if (friendSet.contains(it[0]))
-            friendMap[it[0]] = it
+    return result
+}
+
+private fun friendsScore(
+    friends: List<List<String>>,
+    friendSet: MutableSet<String>,
+    user: String
+): MutableMap<String, Int> {
+    val friendMap = mutableMapOf<String, Int>()
+    for (friend in friends) {
+        val direction = when {
+            friendSet.contains(friend[0]) && user != friend[1] -> friend[1]
+            friendSet.contains(friend[1]) && user != friend[0] -> friend[0]
+            else -> continue
+        }
+        friendMap[direction] = friendMap.getOrDefault(direction, 0) + 10
     }
+    return friendMap
+}
 
-    return listOf()
+private fun sortedByScore(
+    friendMap: MutableMap<String, Int>
+) = friendMap.toList()
+    .sortedByDescending { it.second }
+    .map { it.first }
+    .take(5)
+
+
+private fun visitorScore(
+    visitors: List<String>,
+    friendSet: MutableSet<String>,
+    friendMap: MutableMap<String, Int>
+) {
+    for (visitor in visitors) {
+        if (visitor in friendSet) continue
+        friendMap[visitor] = friendMap.getOrDefault(visitor, 0) + 1
+    }
 }
 
 private fun searchFriends(
-    friends: List<List<String>>, user: String, friendSet: MutableSet<String>
+    friends: List<List<String>>,
+    user: String,
+    friendSet: MutableSet<String>
 ) {
-    friends.forEach {
-        if (it[1] == user || it[0] == user) {
-            friendSet += it[0]
+    friends.forEach { friend ->
+        if (friend[1] == user || friend[0] == user) {
+            friendSet += friend[0]
         }
     }
 }
 
-fun isValidUserName(nickName: String): Boolean = "[a-z]{1,30}".toRegex().matches(nickName)
-
 fun main() {
-    solution7(
-        "mrko", listOf(
-            listOf("donut", "andole"),
-            listOf("donut", "jun"),
-            listOf("donut", "mrko"),
-            listOf("shakevan", "andole"),
-            listOf("shakevan", "jun"),
-            listOf("shan", "donchoi"),
-            listOf("shakevan", "mrko")
-        ), listOf("bedi", "bedi", "donut", "bedi", "shakevan")
+    println(
+        solution7(
+            "mrko", listOf(
+                listOf("donut", "andole"),
+                listOf("donut", "jun"),
+                listOf("donut", "mrko"),
+                listOf("shakevan", "andole"),
+                listOf("shakevan", "jun"),
+                listOf("shan", "donchoi"),
+                listOf("shakevan", "mrko")
+            ), listOf("bedi", "bedi", "donut", "bedi", "shakevan")
+        )
     )
 }
