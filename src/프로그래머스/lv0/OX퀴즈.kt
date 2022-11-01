ceb2984bbabc0463eb0d3692e52cebdccf95cce8
jklom0326 <jklom0326@gmail.com>
2022-11-01 22:56:21 +0900

    programers lv0

--- src/프로그래머스/lv0/OX퀴즈.kt
+++ src/프로그래머스/lv0/OX퀴즈.kt
@@ -1,12 +1,18 @@
 package 프로그래머스.lv0
 
 fun oxQuiz(quiz: Array<String>): Array<String> {
-    var answer: Array<String> = arrayOf<String>()
-    println(quiz.map { it.split(" ").toString() })
-
+    var answer = arrayOf<String>()
+    val quiz = quiz.map { it.split(' ') }.map { it }
+    for (i in quiz) {
+        if (i[1] == "+") {
+            answer += if (i[4].toInt() == i[0].toInt() + i[2].toInt()) "O" else "X"
+        } else {
+            answer += if (i[4].toInt() == i[0].toInt() - i[2].toInt()) "O" else "X"
+        }
+    }
     return answer
 }
 
 fun main() {
-    oxQuiz(arrayOf("3 - 4 = -3", "5 + 6 = 11"))
+   oxQuiz(arrayOf("3 - 4 = -3", "5 + 6 = 11"))
 }
