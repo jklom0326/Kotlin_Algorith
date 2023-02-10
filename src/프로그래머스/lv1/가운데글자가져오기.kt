0f822b7eb0932680fe074e384356e8a354bfa594
jklom0326 <jklom0326@gmail.com>
2023-02-10 23:10:21 +0900

    algorithm

---
+++ src/프로그래머스/lv1/가운데글자가져오기.kt
@@ -0,0 +1,13 @@
+package 프로그래머스.lv1
+
+fun takeMiddle(s: String): String {
+    if (s.length % 2 == 0){
+        return s.slice((s.length / 2 - 1) .. (s.length / 2))
+    }
+    return s[(s.length / 2)].toString()
+}
+
+fun main() {
+    println(takeMiddle("abcde"))
+    println(takeMiddle("qwer"))
+}
