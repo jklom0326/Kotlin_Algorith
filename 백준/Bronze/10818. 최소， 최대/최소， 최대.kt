dca3644f34e371fad99b209622cf33e656c72fcf
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-06-03 14:27:58 +0900

    [Bronze III] Title: 최소, 최대, Time: 876 ms, Memory: 165108 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val arr = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
+
+    println("${min(arr)} ${max(arr)}")
+}
+
+fun min(array: IntArray): Int{
+    var min = array[0]
+    for (i in array.indices){
+        if (min > array[i]){
+            min = array[i]
+        }
+    }
+    return min
+}
+
+fun max(array: IntArray):Int{
+    var max = array[0]
+    for (i in array.indices){
+        if (max < array[i]){
+            max = array[i]
+        }
+    }
+    return max
+}
