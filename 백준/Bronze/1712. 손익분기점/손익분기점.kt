d8610371a2eea1409ae7cf05c069ed4b5c621bfb
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-06-03 14:28:10 +0900

    [Bronze II] Title: 손익분기점, Time: 148 ms, Memory: 20860 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val n = readLine()!!.split(" ").map { it.toInt() }.let {
+        if (it[1] >= it[2]) {
+            println(-1)
+        } else {
+            println(it[0] / (it[2] - it[1]) + 1)
+        }
+    }
+}
