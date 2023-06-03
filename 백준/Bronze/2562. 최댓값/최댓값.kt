65360edee759aa31c8101e08450e34513d3d896d
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-06-03 14:28:20 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 12240 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var maxNum = 0
+    var maxLine = 0
+
+    for (i in 0 until 9) {
+        val inputNum = readLine()!!.toInt()
+
+        if (maxNum < inputNum) {
+            maxNum = inputNum
+            maxLine = i
+        }
+    }
+
+    println("$maxNum\n${maxLine + 1}")
+}
