f4697426957a47ced9a7cd0a1e96a78bb399e62c
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-06-26 21:31:28 +0900

    [Bronze II] Title: 소수 찾기, Time: 132 ms, Memory: 20640 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.kt
@@ -0,0 +1,23 @@
+import kotlin.math.sqrt
+
+fun main() {
+    val n = readLine()!!.toInt()
+    val arr = readLine()!!.split(" ").map { it.toInt() }
+    var count = 0
+    for (i in 0 until n){
+        if (prime(arr[i])){
+            count++
+        }
+    }
+    println(count)
+}
+
+fun prime(num: Int): Boolean {
+    if (num == 1){
+        return false
+    }
+    for (i in 2 .. sqrt(num.toDouble()).toInt()) {
+        if (num % i == 0) return false
+    }
+    return true
+}
