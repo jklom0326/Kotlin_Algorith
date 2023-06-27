4684e495db267c21c25b682572a003391692dea9
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2023-06-27 22:26:41 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 12272 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val n = readLine()!!.toInt()
+    val len = n.toString().length
+    var result = 0
+
+    for (i in (n - (len * 9)) until n){
+        var sum = 0
+        var num = i
+
+        while (num != 0){
+            sum += num % 10
+            num /= 10
+        }
+        if ((sum + i) == n){
+            result = i
+            break
+        }
+    }
+    println(result)
+}
