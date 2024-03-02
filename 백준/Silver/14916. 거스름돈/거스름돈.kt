b93051be015cbe106ee3a9399622d3598bd96422
Sangwan Han <49683156+jklom0326@users.noreply.github.com>
2024-03-02 22:02:58 +0900

    [Silver V] Title: 거스름돈, Time: 96 ms, Memory: 12332 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var n = readln().toInt()
+    var count = 0
+
+    while (n > 0) {
+        if (n % 5 == 0) {
+            count += n / 5
+            break
+        }
+        n -= 2
+        count++
+    }
+    if (n < 0) println(-1) else println(count)
+}
